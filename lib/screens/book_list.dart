0a371fdad530feb08e8090d65cba737fced3c634
Gayanga <gayangabandara74@gmail.comgit commit -am initial>
2025-01-13 15:56:59 +0530

    Add to price and Add to cart oprion

--- lib/screens/book_list.dart
+++ lib/screens/book_list.dart
@@ -4,23 +4,23 @@ import '../widgets/book.dart';
 class BookListScreen extends StatelessWidget {
   final List<Map<String, String>> books = [
     {
-      'title': 'Book 1',
-      'author': 'Author 1',
-      'price': '\$10',
+      'title': 'Sandstorm',
+      'author': 'James Rollins',
+      'price': '1450',
       'image': 'assets/images/book1.jpg',
-      'description': 'This is the description of Book 1.'
+      'description': 'A heart-stopping novel of adventure and suspense from #1 New York Times bestselling author James Rollins—the first in his critically acclaimed Sigma Force series A freak explosion in the British Museum in London ignites a perilous race for an earth-shaking power source buried deep beneath the sands of history. Painter Crowe is an agent for Sigma Force, a covert arm of the Defense Department tasked with keeping dangerous scientific discoveries out of enemy hands. When an ancient artifact points the way toward the legendary "Atlantis of the Sands," Painter must travel across the world in search of the lost city and a destructive power beyond imagining.'
     },
     {
       'title': 'Book 2',
       'author': 'Author 2',
-      'price': '\$12',
+      'price': '1250',
       'image': 'assets/images/book2.jpg',
       'description': 'This is the description of Book 2.'
     },
     {
       'title': 'Book 3',
       'author': 'Author 3',
-      'price': '\$15',
+      'price': '1500',
       'image': 'assets/images/book3.jpg',
       'description': 'This is the description of Book 3.'
     }
--- lib/widgets/book.dart
+++ lib/widgets/book.dart
@@ -7,7 +7,8 @@ class BookWidget extends StatelessWidget {
   final String imagePath;
   final String description;
 
-  const BookWidget({super.key, 
+  const BookWidget({
+    super.key,
     required this.title,
     required this.author,
     required this.price,
@@ -17,71 +18,82 @@ class BookWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              title,
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center, // Centering title
+    return Container(
+      color: Colors.blue[50], // Light blue background for the bookstore
+      child: Card(
+        margin: EdgeInsets.all(5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center, // Centering title
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              'by $author',
-              style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 158, 158, 158)),
-              textAlign: TextAlign.center, // Centering author name
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                'by $author',
+                style: TextStyle(fontSize: 16, color: const Color.fromARGB(255, 158, 158, 158)),
+                textAlign: TextAlign.center, // Centering author name
+              ),
             ),
-          ),
-          GestureDetector(
-            onTap: () {
-              // Show the description when the image is clicked
-              showDialog(
-                context: context,
-                builder: (context) => AlertDialog(
-                  title: Text(title),
-                  content: Text(description),
-                  actions: [
-                    TextButton(
-                      child: Text('Close'),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ],
-                ),
-              );
-            },
-            child: Image.asset(
-              imagePath,
-              height: 250, // Adjusted height for portrait mode
-              width: 150, // Adjusted width for centered display
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              price,
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center, // Centering price
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Add to cart functionality here
+            GestureDetector(
+              onTap: () {
+                // Show the description when the image is clicked
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(title),
+                    content: Text(description),
+                    actions: [
+                      TextButton(
+                        child: Text('Close'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                );
               },
-              child: Text('Add to Cart'),
+              child: Image.asset(
+                imagePath,
+                height: 250, // Adjusted height for portrait mode
+                width: 150, // Adjusted width for centered display
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center, // Centers the items
+                children: <Widget>[
+                  Text(
+                    price,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(width: 20, height: 50), // Adds more spacing between price and button
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(48, 33, 149, 243), // Blue color for the button
+                    ),
+                    onPressed: () {
+                      // Add to cart functionality here
+                    },
+                    child: Text(
+                      'Add to Cart',
+                      style: TextStyle(fontWeight: FontWeight.bold), // Bold text for button
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
